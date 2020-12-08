cafd3a3d6383ebbd6f99d4f4d44cbf65605741cf
Pedro Correia <pedro.correia.105@gmail.com>
2020-12-08 12:13:42 +0000

    Day 9

---
+++ 2020/09.py
@@ -0,0 +1,24 @@
+from aocd import get_data
+
+data = list(map(int, get_data(day=9, year=2020).splitlines()))
+pointer = 25
+vulnerability = False
+
+while not vulnerability:
+	vulnerability = data[pointer]
+	for lower in range(pointer - 25, pointer - 1):
+		for upper in range(lower, pointer):
+			if data[lower] + data[upper] == data[pointer]:
+				vulnerability = False
+	pointer += 1
+
+print(vulnerability)
+
+for lower in range(len(data)):
+	total = data[lower]
+	for upper in range(lower + 1, len(data)):
+		total += data[upper]
+		if total == vulnerability:
+			print(min(data[lower:upper + 1]) + max(data[lower:upper + 1]))
+		elif total > vulnerability:
+			break
